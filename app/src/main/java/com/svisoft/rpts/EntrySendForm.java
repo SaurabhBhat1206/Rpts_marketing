b0c2058e3761c984b5b0131d57484e36daa892ee
saurabh.bhat1206@gmail.com <saurabh.bhat1206@gmail.com>
2016-12-22 10:52:15 +0530

    new

---
+++ app/src/main/java/com/svisoft/rpts/EntrySendForm.java
@@ -0,0 +1,13 @@
+package com.svisoft.rpts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EntrySendForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entry_send_form);
+    }
+}
---
+++ app/src/main/java/com/svisoft/rpts/ExitSendForm.java
@@ -0,0 +1,13 @@
+package com.svisoft.rpts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExitSendForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exit_send_form);
+    }
+}
